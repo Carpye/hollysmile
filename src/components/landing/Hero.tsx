ae7a581122c2f2b7b9fe09f6d5dba05c1dc2057f
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2024-09-06 17:53:04 +0200

    style: fixed hero and about us

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,86 +1,59 @@
 "use client"
-import { useRef } from "react"
-import { motion } from "framer-motion"
+import { useEffect, useState } from "react"
 import Image from "next/image"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel"
-import Autoplay from "embla-carousel-autoplay"
-import Fade from "embla-carousel-fade"
 import { Button } from "../ui/button"
 import { ChevronDown } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 const images = [
   "/assets/grid-background.png",
   "/assets/polkadot-background.png",
-  "temporarybg.svg",
+  "/temporarybg.svg",
 ]
 
 const Hero = () => {
-  const fadePlugin = useRef(Fade())
-  const autoplayPlugin = useRef(Autoplay({ delay: 3000 }))
+  const [currentSlide, setCurrentSlide] = useState(images.length)
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length)
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentSlide(0)
+    }, 100)
+  }, [])
   return (
-    <div className="relative h-[calc(100vh-128px)] max-h-[600px] w-full overflow-hidden">
-      <Carousel
-        opts={{
-          loop: true,
-          align: "center",
-          dragFree: false,
-          watchDrag: false,
-        }}
-        plugins={[fadePlugin.current, autoplayPlugin.current]}
-      >
-        <CarouselContent>
-          {images.map((image, index) => {
-            const divRef = useRef<HTMLDivElement>(null)
-
-            return (
-              <CarouselItem
-                key={index}
-                className="h-[600px] max-h-[600px] w-full overflow-hidden"
-              >
-                <motion.div
-                  ref={divRef}
-                  className="relative flex h-full w-full items-center justify-center"
-                  onViewportEnter={() => {
-                    console.log(`Image ${index} entered viewport`)
-                    if (divRef.current) {
-                      divRef.current.classList.add("zoom-animation")
-                    }
-                  }}
-                  onViewportLeave={() => {
-                    if (divRef.current) {
-                      divRef.current.classList.remove("zoom-animation")
-                    }
-                  }}
-                >
-                  <Image
-                    src={image}
-                    fill
-                    alt={`Image ${index}`}
-                    className="zoom-animation object-cover"
-                  />
-                </motion.div>
-              </CarouselItem>
-            )
-          })}
-        </CarouselContent>
-      </Carousel>
-      <div className="absolute bottom-0 left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-end gap-8">
-        <h1 className="flex flex-col text-center text-5xl font-semibold">
+    <div className="relative h-screen max-h-[600px] w-full overflow-hidden">
+      {images.map((image, index) => (
+        <Image
+          key={image}
+          src={image}
+          alt={`Slide ${index}`}
+          fill
+          className={cn(
+            "slide object-cover",
+            currentSlide == index ? "active" : ""
+          )}
+        />
+      ))}
+      <div className="absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-end gap-8 px-2 sm:w-3/4 lg:w-1/2">
+        <h1 className="flex flex-col text-center text-4xl font-semibold sm:text-5xl xl:text-6xl">
           Zadbaj o swój piękny <span className="text-primary">Uśmiech</span>
         </h1>
-        <p className="flex w-4/5 flex-col items-center justify-center text-center text-zinc-500">
+        <p className="flex w-3/4 flex-col items-center justify-center text-center text-zinc-500">
           Piękny uśmiech zaczyna się od najlepszych szczoteczek – odkryj naszą
           wyjątkową ofertę w <span className="text-primary">Holly Smile!</span>
         </p>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Button className="shadow-xl">Zobacz naszą ofertę!</Button>
-          <ChevronDown className="animate-bounce" />
-        </div>
+      </div>
+      <div className="absolute bottom-2 left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-center gap-4">
+        <Button className="shadow-xl" size={"lg"}>
+          Zobacz naszą ofertę!
+        </Button>
+        <ChevronDown className="z-50 h-12 w-12 animate-bounce text-neutral-700" />
       </div>
     </div>
   )
