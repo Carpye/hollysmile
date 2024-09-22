ccc7ab05baa69edad53fa4af3efee04b8334f61e
Kacper Kozłowski <kati8ko@gmail.com>
2024-09-23 01:31:54 +0200

    in progress

--- src/app/api/lemonsqueezy/route.ts
+++ src/app/api/lemonsqueezy/route.ts
@@ -1,18 +1,24 @@
-import { lemonSqueezyApiInstance } from "@/lib/axios";
-import { NextRequest, NextResponse } from "next/server";
+import { lemonSqueezyApiInstance } from "@/lib/axios"
+import { NextRequest, NextResponse } from "next/server"
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"
 
 export async function POST(req: NextRequest) {
   try {
+    const reqData = await req.json()
 
-    const reqData = await req.json();
+    console.log(reqData)
 
-    if (!reqData.productId) {
-      console.error("Product ID is missing");
-      return NextResponse.json({ message: "Product ID is required" }, { status: 400 });
+    const productId = "526538"
+    // reqData.items[0].productId.toString()
+
+    if (!productId) {
+      console.error("Product ID is missing")
+      return NextResponse.json(
+        { message: "Product ID is required" },
+        { status: 400 }
+      )
     }
-
 
     const response = await lemonSqueezyApiInstance.post("/checkouts", {
       data: {
@@ -34,18 +40,22 @@ export async function POST(req: NextRequest) {
           variant: {
             data: {
               type: "variants",
-              id: reqData.productId.toString(),
+              // id: reqData.items[0].productId.toString(),
+              id: productId,
             },
           },
         },
       },
-    });
+    })
 
-    const checkoutUrl = response.data.data.attributes.url;
-    console.log(response.data);
-    return NextResponse.json({ checkoutUrl });
+    const checkoutUrl = response.data.data.attributes.url
+
+    console.log(response.data)
+
+    return NextResponse.json(response.data.data.attributes, { status: 200 })
   } catch (error) {
-    console.error("Error in POST /api/lemonsqueezy:", error);
-    return NextResponse.json({ message: "An error occured" }, { status: 500 });
+    console.error("Error in POST /api/lemonsqueezy:", error)
+
+    return NextResponse.json({ message: "An error occured" }, { status: 500 })
   }
 }
